eadd967eb4f0e062aebb4824cc520109f603b4ad
SavchenkoAS <artt.sav@icloud.com>
2020-09-05 01:16:40 +0300

    Программа сложения всех цифры

---
+++ src/X5/Work2.java
@@ -0,0 +1,13 @@
+package X5;
+
+import java.util.Scanner;
+
+public class Work2 {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        System.out.print("Input a number:");
+        int i = num.nextInt();
+        System.out.println((i % 10) + (i / 10) % 10 + (i /100));
+    }
+
+}
