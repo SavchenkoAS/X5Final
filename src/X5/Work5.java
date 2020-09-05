6a76c8b5d744631b1a54e5e01b1b2f8fd3237864
SavchenkoAS <artt.sav@icloud.com>
2020-09-05 19:32:31 +0300

    Нахождение наибольшего числа

---
+++ src/X5/Work5.java
@@ -0,0 +1,33 @@
+package X5;
+
+import java.util.Scanner;
+
+public class Work5 {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+            System.out.print("Input a number:");
+        int i = num.nextInt();{
+            System.out.print("Input a number:");
+        }
+        int b = num.nextInt();{
+            System.out.print("Input a number:");
+        }
+        int v = num.nextInt();{
+                if (i > b)
+                if (i > v) {
+                    System.out.print("Max number:" + i);
+            }
+                if (b > i)
+                if (b > v) {
+                    System.out.print("Max number:" + b);
+                }
+                if (v > i)
+                if (v > b) {
+                    System.out.print("Max number:" + v);
+                }
+                }
+        }
+    }
+
+
+
