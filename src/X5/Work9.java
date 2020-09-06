d1e98d30c29b51855394c7cb90058f4e5950f4a5
SavchenkoAS <artt.sav@icloud.com>
2020-09-06 15:04:02 +0300

    Массив в обратном порядке

---
+++ src/X5/Work9.java
@@ -0,0 +1,14 @@
+package X5;
+
+public class Work9 {
+    public static void main(String[] args) {
+        int[] numbers = {15, 10, 0, -6, -5, 3, 0, -34, 0, 32, 56, 0, 24, -52};
+        {
+            for (int i = numbers.length - 1; i >= 0; i--) {
+
+                System.out.print(numbers[i] + "," + " ");
+
+            }
+        }
+    }
+}
